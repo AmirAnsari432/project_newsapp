cd63699ddf937f5f54d94dc1df47b366288ae002
Amir <amirbhai432@gmail.com>
2022-11-25 22:37:17 +0530

    change navbar

--- src/Component/NavBar.jsx
+++ src/Component/NavBar.jsx
@@ -4,8 +4,10 @@ import {  Link } from "react-router-dom";
 
 const Navbar =() => {
 
+  
+
     return (
-      <nav className="navbar fixed-top navbar-expand-lg bg-light">
+      <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
           <Link className="navbar-brand" to="/">
             NewsMonkey
